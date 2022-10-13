eae955ed8521dd2eceaea6e4c9261217125212a7
Naresh Pardeshi <naresh.pardeshi@calsoftinc.com>
2022-10-13 18:39:52 +0530

    First commit

---
+++ src/test/java/com/springbootmongodb/docker/SpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmongodb.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
